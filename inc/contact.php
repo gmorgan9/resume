111b182ebbe8a8a321c2273f4ae80023d75c86a2
Garrett Morgan <54009016+gmorgan9@users.noreply.github.com>
2022-03-16 21:52:19 -0600

    Add files via upload

---
+++ inc/contact.php
@@ -0,0 +1,29 @@
+<?php 
+  //creating connection to database
+$con=mysqli_connect("localhost","developer","BIGmorgan1999!","resumesite") or die(mysqli_error());
+
+  //check whether submit button is pressed or not
+if((isset($_POST['submit'])))
+{
+  //fetching and storing the form data in variables
+$Name = $con->real_escape_string($_POST['Name']);
+$Email = $con->real_escape_string($_POST['Email']);
+$Subject = $con->real_escape_string($_POST['Subject']);
+$Message = $con->real_escape_string($_POST['Message']);
+
+  //query to insert the variable data into the database
+$sql="INSERT INTO contact_form (Name, Email, Subject, Message) VALUES ('".$Name."','".$Email."', '".$Subject."', '".$Message."')";
+
+  //Execute the query and returning a message
+if(!$result = $con->query($sql)){
+die('Error occured [' . $con->error . ']');
+}
+else
+
+   echo "Thank you! I will get in touch with you soon";
+   header('Location: ../index.html');
+	exit;
+}
+
+
+?>
